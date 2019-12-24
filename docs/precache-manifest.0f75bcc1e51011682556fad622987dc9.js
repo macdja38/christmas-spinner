10e108d5c4f278c16f37d5915ee26054f1c9cf41
macdja38 <jakeincanada@icloud.com>
2019-12-24 02:06:41 -0500

    Write christmas spinner code

---
+++ docs/precache-manifest.0f75bcc1e51011682556fad622987dc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff5606d58adb8f6e2cb085aeb1ebe74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e025f4b3d2b02f5d8a2c",
+    "url": "/static/css/main.ce691deb.chunk.css"
+  },
+  {
+    "revision": "137aac7ef07198f4a2e8",
+    "url": "/static/js/2.b7ae5ef0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b7ae5ef0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e025f4b3d2b02f5d8a2c",
+    "url": "/static/js/main.22c4c934.chunk.js"
+  },
+  {
+    "revision": "e5f92852a84db3bb7b12",
+    "url": "/static/js/runtime-main.0f6ceff3.js"
+  },
+  {
+    "revision": "ac939d72d18845f74f3550fc9b53ff2a",
+    "url": "/static/media/Spinner_Horse_Race_Game.ac939d72.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import spinner from './Spinner_Horse_Race_Game.png';
 import './App.css';
+import Wheel from 'lottery-wheel/dist/lottery-wheel';
+import anime from 'animejs/lib/anime.es.js';
+
+const prizes = [];
+
+for (let i = 0; i++; i < 6) {
+  for (let j of [2, 3, 5, 3, 2]) {
+    prizes.push(`${i}:${j}`)
+  }
+}
+
+window.delay = 8000;
+
+let counter = 0;
+
+function getNewRotations() {
+  return counter += Math.random() * 3 + 1
+}
+
+function spin() {
+  anime({
+    targets: '#wheel',
+    rotate: `${getNewRotations()}turn`,
+    duration: window.delay || 8000,
+  });
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{margin: 0, padding: 0, transform: "translate(0, 2vh)"}}>↓</p>
+        <img id="wheel" src={spinner} className="App-logo" alt="logo" />
+        <button onClick={spin}>Spin</button>
       </header>
     </div>
   );
